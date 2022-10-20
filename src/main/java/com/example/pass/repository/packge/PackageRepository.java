eb98975a53c007e9baa74a5c2fe833afadf2b9e8
jisu3316 <jisu3316@naver.com>
2022-10-20 22:57:50 +0900

    jpa, mysql 연동 코드 작성 중

---
+++ src/main/java/com/example/pass/repository/packge/PackageRepository.java
@@ -0,0 +1,6 @@
+package com.example.pass.repository.packge;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+}
